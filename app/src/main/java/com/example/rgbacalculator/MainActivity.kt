4106ffb13aa36ee8f096c32ff362016bd5151197
thaisar <silveira.thaisaraujo@gmail.com>
2019-02-25 21:49:19 -0300

    PrimeiroLayout
    Layout para testes feito

--- app/src/main/java/com/example/rgbacalculator/MainActivity.kt
+++ app/src/main/java/com/example/rgbacalculator/MainActivity.kt
@@ -2,8 +2,19 @@ package com.example.rgbacalculator
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.SeekBar
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var EditText1: EditText
+    private lateinit var EditText2: EditText
+    private lateinit var EditText3: EditText
+    private lateinit var SeekBar1: SeekBar
+    private lateinit var SeekBar2: SeekBar
+    private lateinit var SeekBar3: SeekBar
+    private lateinit var ImageView: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
